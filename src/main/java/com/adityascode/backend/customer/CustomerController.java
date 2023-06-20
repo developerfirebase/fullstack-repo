384a67f5e8db59f16036fec4902573cee8d1f7df
myname <developerfirebase18@gmail.com>
2023-06-20 16:04:28 +0530

    Initial committ-- Till Crud Ops

---
+++ src/main/java/com/adityascode/backend/customer/CustomerController.java
@@ -0,0 +1,34 @@
+package com.adityascode.backend.customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+
+@RestController
+@RequestMapping("api/v1/customers")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+    return customerService.getAllCustomers();
+}
+//    @GetMapping("/{customerId}")
+//    public Customer getCustomer(@PathVariable ("customerId") Integer cusromerId){
+//    customers.stream()
+//            .filter(c -> c.getId().equals(customerId))
+//            .findFirst()
+//             )
+//        return customer;
+//    }
+
+}
---
+++ src/main/java/com/adityascode/backend/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,24 @@
+package com.adityascode.backend.customer;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jpa")
+public class CustomerJPADataAccessService implements CustomerDao {
+    private final CustomerRepository customerRepository;
+
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+@Override
+    public List<Customer> selectAllCustomers() {
+        return customerRepository.findAll();
+}
+//@Override
+//    public Optional<Customer> selectCustomerById(){
+//        return Optional.empty();
+//}
+}
